81edf8f05fb8404145170cb4d348aec2974d3f86
pavan <jakkulapavankalyan02@gmail.com>
2024-02-03 23:51:45 +0530

    Initial commit

---
+++ 1.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255),
+        Ciiity varchar(255)
+);
+
+INSERT INTO Persons(
+    LastName,FirstName
+)
+Values{
+    'Jakkila','Pavan'
+}
